82b8b9c1f0f614edebe6368c8eda30dc16de78a7
zhanxiaohang <zhanxiaohang@sensetime.com>
2019-04-26 18:59:16 +0800

    update

--- experiments/semiauto_annot/resnet50_vip+mpii/resume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-work_path=$(dirname $0)
-GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p VI_SP_VA_1080TI -x SH-IDC1-10-5-36-37 -n8 --gres=gpu:8 --ntasks-per-node=8 \
-python -u main.py \
-    --config $work_path/config.yaml \
-    --load-iter 42000 \
-    --load-cmp-only \
-    --resume
